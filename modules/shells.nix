e8e4ea86cab1f7e27718029f9c01b2ba39b59482
Invra <identificationsucks@gmail.com>
2026-01-29 13:19:20 +0000

    add: fastfetch

--- modules/shells.nix
+++ modules/shells.nix
@@ -27,93 +27,190 @@ in
     home.file.".hushlogin".text = "";
 
     stylix.targets.fish.enable = false;
-    programs.fish = {
-      enable = true;
-      shellAliases = {
-        l = "ls -l";
-        la = "ls -al";
-        ":q" = "exit";
-        fuckoff = "exit";
-        edit = "taskset -c 0-7 hx";
+    programs = {
+      fish = {
+        enable = true;
+        shellAliases = {
+          l = "ls -l";
+          la = "ls -al";
+          ":q" = "exit";
+          fuckoff = "exit";
+          edit = "taskset -c 0-7 hx";
+        };
+
+        interactiveShellInit = ''
+          #!/bin/env fish
+
+          set fish_greeting ""
+          alias tree "eza --tree"
+
+          if status is-interactive
+            if not set -q TMUX
+              tmux
+            end
+          end
+        '';
       };
 
-      interactiveShellInit = ''
-        #!/bin/env fish
+      eza = {
+        enable = true;
+        enableFishIntegration = true;
+        icons = "always";
+      };
 
-        set fish_greeting ""
-        alias tree "eza --tree"
+      starship = {
+        enable = true;
+        enableFishIntegration = true;
+        settings = {
+          character = {
+            error_symbol = "[ 󱞪](bold red)";
+            success_symbol = "[ 󱞪](bold green)";
+            vimcmd_replace_one_symbol = "[<](bold purple)";
+            vimcmd_replace_symbol = "[<](bold purple)";
+            vimcmd_symbol = "[<](bold green)";
+            vimcmd_visual_symbol = "[<](bold yellow)";
+          };
+          continuation_prompt = "[.](bright-black) ";
+          format = "$directory$git_branch$git_status$bun$deno$rust$golang$haskell$haxe$zig$c$cpp$cmake$swift$dotnet$nix_shell$time\n$character";
+          bun.symbol = "bun ";
+          c.symbol = "c ";
+          cmake.symbol = "cmake ";
+          cpp.symbol = "c++ ";
+          deno.symbol = "deno ";
+          directory.read_only = " ro";
+          dotnet = {
+            format = "via [$symbol($version )(target $tfm )]($style)";
+            symbol = ".net ";
+          };
+          git_branch = {
+            symbol = "git ";
+            truncation_symbol = "...";
+          };
+          git_commit.tag_symbol = " tag ";
+          git_status = {
+            ahead = ">";
+            behind = "<";
+            deleted = "x";
+            diverged = "<>";
+            renamed = "r";
+          };
+          haskell.symbol = "haskell ";
+          haxe.symbol = "haxe ";
+          nix_shell.symbol = "nix ";
+          package.symbol = "pkg ";
+          rust.symbol = "rust ";
+          swift.symbol = "swift ";
+          zig.symbol = "zig ";
+        };
+      };
 
-        if status is-interactive
-          if not set -q TMUX
-            tmux
-          end
-        end
-      '';
-    };
+      carapace = {
+        enable = true;
+        enableZshIntegration = true;
+        enableFishIntegration = true;
+      };
 
-    programs.eza = {
-      enable = true;
-      enableFishIntegration = true;
-      icons = "always";
+      zoxide = {
+        enable = true;
+        enableFishIntegration = true;
+        options = [ "--cmd cd" ];
+      };
+
+      fastfetch = {
+        enable = true;
+
+        settings = {
+          logo.source = "nixos";
+          display = {
+          size.binaryPrefix = "si";
+            color = "blue";
+            separator = " ";
+          };
+          modules = [
+            {
+              type = "os";
+              key = "os   ";
+              keyColor = "blue";
+              format = "{name} {version}";
+            }
+            {
+              type = "kernel";
+              key = "krnl ";
+              keyColor = "blue";
+            }
+            {
+              type = "packages";
+              key = "pkgs ";
+              keyColor = "blue";
+            }
+            {
+              type = "shell";
+              key = "shell";
+              keyColor = "blue";
+            }
+            "break"
+            {
+              type = "wm";
+              key = "wm   ";
+              keyColor = "red";
+            }
+            {
+              type = "terminal";
+              key = "term ";
+              keyColor = "red";
+            }
+            {
+              type = "font";
+              key = "font ";
+              keyColor = "red";
+            }
+            {
+              type = "icons";
+              key = "icon ";
+              keyColor = "red";
+            }
+            "break"
+            {
+              type = "board";
+              key = "pc   ";
+              keyColor = "green";
+            }
+            {
+              type = "cpu";
+              key = "cpu  ";
+              keyColor = "green";
+            }
+            {
+              type = "memory";
+              key = "mem  ";
+              keyColor = "green";
+            }
+            {
+              type = "gpu";
+              key = "gpu  ";
+              keyColor = "green";
+            }
+            {
+              type = "disk";
+              key = "disk ";
+              keyColor = "green";
+            }
+            "break"
+            {
+              type = "localip";
+              key = "ip   ";
+              keyColor = "yellow";
+            }
+            {
+              type = "dns";
+              key = "dns  ";
+              keyColor = "yellow";
+            }
+          ];
+        };
+      };
     };
 
     stylix.targets.starship.enable = false;
-    programs.starship = {
-      enable = true;
-      enableFishIntegration = true;
-      settings = {
-        character = {
-          error_symbol = "[ 󱞪](bold red)";
-          success_symbol = "[ 󱞪](bold green)";
-          vimcmd_replace_one_symbol = "[<](bold purple)";
-          vimcmd_replace_symbol = "[<](bold purple)";
-          vimcmd_symbol = "[<](bold green)";
-          vimcmd_visual_symbol = "[<](bold yellow)";
-        };
-        continuation_prompt = "[.](bright-black) ";
-        format = "$directory$git_branch$git_status$bun$deno$rust$golang$haskell$haxe$zig$c$cpp$cmake$swift$dotnet$nix_shell$time\n$character";
-        bun.symbol = "bun ";
-        c.symbol = "c ";
-        cmake.symbol = "cmake ";
-        cpp.symbol = "c++ ";
-        deno.symbol = "deno ";
-        directory.read_only = " ro";
-        dotnet = {
-          format = "via [$symbol($version )(target $tfm )]($style)";
-          symbol = ".net ";
-        };
-        git_branch = {
-          symbol = "git ";
-          truncation_symbol = "...";
-        };
-        git_commit.tag_symbol = " tag ";
-        git_status = {
-          ahead = ">";
-          behind = "<";
-          deleted = "x";
-          diverged = "<>";
-          renamed = "r";
-        };
-        haskell.symbol = "haskell ";
-        haxe.symbol = "haxe ";
-        nix_shell.symbol = "nix ";
-        package.symbol = "pkg ";
-        rust.symbol = "rust ";
-        swift.symbol = "swift ";
-        zig.symbol = "zig ";
-      };
-    };
-
-    programs.carapace = {
-      enable = true;
-      enableZshIntegration = true;
-      enableFishIntegration = true;
-    };
-
-    programs.zoxide = {
-      enable = true;
-      enableFishIntegration = true;
-      options = [ "--cmd cd" ];
-    };
   };
 }
