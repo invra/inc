54c69290f9c9a80c6babde649285d05f703cf5ab
Invra <identificationsucks@gmail.com>
2026-02-19 03:13:39 +1100

    chore: format

--- modules/security.nix
+++ modules/security.nix
@@ -1,27 +1,25 @@
 { config, ... }:
 {
-  flake.modules.nixos.base =
-    { pkgs, ... }:
-    {
-      security = {
-        rtkit.enable = true;
-        sudo.enable = false;
-        doas = {
-          enable = true;
-          extraRules = [
-            {
-              users = [ config.flake.meta.owner.username ];
-              keepEnv = true;
-              persist = true;
-            }
-          ];
-        };
+  flake.modules.nixos.base = {
+    security = {
+      rtkit.enable = true;
+      sudo.enable = false;
+      doas = {
+        enable = true;
+        extraRules = [
+          {
+            users = [ config.flake.meta.owner.username ];
+            keepEnv = true;
+            persist = true;
+          }
+        ];
       };
-      users.users.${config.flake.meta.owner.username}.extraGroups = [
-        "wheel"
-        "systemd-journal"
-      ];
     };
+    users.users.${config.flake.meta.owner.username}.extraGroups = [
+      "wheel"
+      "systemd-journal"
+    ];
+  };
 
   flake.modules.darwin.base = {
     security.pam.services.sudo_local.touchIdAuth = true;
