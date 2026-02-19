a26b7c88aa0a41caffe7a1abf0f16dc5ddb19cc5
Invra <identificationsucks@gmail.com>
2026-02-19 16:00:07 +1100

    fix: font stuff

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -6,9 +6,8 @@ let
         lilex
         ibm-plex
         noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
         noto-fonts-color-emoji
+        nerd-fonts.jetbrains-mono
       ];
     };
 in
