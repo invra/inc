0825162e95a5b904a6ba982d827f37689b083fc3
Invra <identificationsucks@gmail.com>
2026-03-02 13:54:42 +1100

    chore: fix config

--- hosts/vdam-x86/default.nix
+++ hosts/vdam-x86/default.nix
@@ -1,15 +1,50 @@
 { config, ... }:
+let
+  this = config;
+in
 {
-  configurations.vdam-x86.module = {
-    imports = with config.flake.modules.nixos; [ base ];
+  configurations.pc-x86.module = {
+    imports = with this.flake.modules.nixos; [
+      base
+    ];
     networking = {
       hostId = "0e8e163d";
       hostName = "NixOS";
     };
     facter.reportPath = ./facter.json;
-    hardware.nvidia.open = false;
+
+    boot.initrd = {
+      kernelModules = [
+        "xhci_hcd"
+        "xhci_pci"
+        "usbhid"
+        "hid_generic"
+        "nvme"
+        "ahci"
+        "sd_mod"
+        "kvm-amd"
+      ];
+
+      availableKernelModules = [
+        "usb_storage"
+        "thunderbolt"
+      ];
+    };
+
+    services.xserver.videoDrivers = [
+      "modesetting"
+      "amdgpu"
+    ];
+
     fileSystems = {
-      "/".device = "/dev/vda2";
+      "/" = {
+        device = "/dev/vda2";
+        fsType = "ext4";
+      };
+    };
+    services = {
+      qemuGuest.enable = true;
+      spice-vdagentd.enable = true;
     };
     system.stateVersion = "24.11";
   };
