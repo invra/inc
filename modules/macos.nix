be41526735e069282edbd906df16a3e01c071aa0
Invra <identificationsucks@gmail.com>
2026-03-27 00:53:43 +1100

    add: aerospace for darwin

---
+++ modules/macos.nix
@@ -0,0 +1,122 @@
+{ lib, ... }:
+{
+  flake.modules.homeManager.base =
+    { darwin, ... }:
+    lib.optionalAttrs darwin {
+      targets.darwin = {
+        defaults = {
+          # This SHOULD disable all screenshot keybinds?
+          "com.apple.symbolichotkeys".AppleSymbolicHotKeys = {
+            "28" = {
+              enabled = false;
+              value = {
+                parameters = [51 20 1179648];
+                type = "standard";
+              };
+            };
+            "29" = {
+              enabled = false;
+              value = {
+                parameters = [51 20 1441792];
+                type = "standard";
+              };
+            };
+            "30" = {
+              enabled = false;
+              value = {
+                parameters = [52 21 1179648];
+                type = "standard";
+              };
+            };
+            "31" = {
+              enabled = false;
+              value = {
+                parameters = [52 21 1441792];
+                type = "standard";
+              };
+            };
+            "184" = {
+              enabled = false;
+              value = {
+                parameters = [53 23 1179648];
+                type = "standard";
+              };
+            };
+          };
+
+          NSGlobalDomain = {
+            AppleShowAllExtensions = false;
+            # Cmd + Ctrl to drag from window anywhere not needing it's chrome
+            NSWindowShouldDragOnGesture = true;
+            NSAutomaticSpellingCorrectionEnabled = false;
+            # Default save files to disk not iCloud
+            NSDocumentSaveNewDocumentsToCloud = false;
+            # Force set icon size to default value
+            NSTableViewDefaultSizeMode = 2;
+            # Singular enum, of value Dark. https://mynixos.com/nix-darwin/option/system.defaults.NSGlobalDomain.AppleInterfaceStyle
+            AppleInterfaceStyle = "Dark";
+            AppleHighlightColor = "0.968627 0.831373 1.000000 Purple";
+            AppleAccentColor = 5;
+          };
+
+          "com.apple.screencapture" = {
+            location = "~/Pictures";
+            type = "png";
+            target = "clipboard";
+            disable-shadow = true;
+          };
+
+          "com.apple.WindowManager" = {
+            EnableStandardClickToShowDesktop = false;
+            GloballyEnabled = false;
+          };
+
+          "com.apple.menuextra.clock" = {
+            Show24Hour = true;
+            ShowSeconds = true;
+          };
+
+          "com.apple.desktopservices" = {
+            DSDontWriteNetworkStores = true;
+            DSDontWriteUSBStores = true;
+          };
+
+          "com.apple.finder" = {
+            AppleShowAllExtensions = true;
+            # List view
+            FXPreferredViewStyle = "Nlsv";
+            # Search the current folder
+            FXDefaultSearchScope = "SCcf";
+            # List with directories at first
+            _FXSortFoldersFirst = true;
+            # Disable finder tabs (due to WM)
+            FinderSpawnTab = false;
+            # Delete bin in 30 days
+            FXRemoveOldTrashItems = true;
+            # Disable change fs-extension warning
+            FXEnableExtensionChangeWarning = false;
+            showWindowTitlebarIcons = false;
+            NSToolbarTitleViewRolloverDelay = 0.2;
+            AppleShowAllFiles = true;
+            ShowPathbar = true;
+            CreateDesktop = false;
+            QuitMenuItem = true;
+            NewWindowTarget = "Home";
+          };
+
+          "com.apple.dock" = {
+            autohide = true;
+            orientation = "bottom";
+            tilesize = 32;
+            minimize-to-application = true;
+            show-recents = false;
+            wvous-br-corner = 1;
+            size-immutable = true;
+          };
+
+          # Disable AI bullshit
+          "com.apple.CloudSubscriptionFeatures.optIn"."545129924" = false;
+        };
+      };
+    };
+}
---
+++ modules/window-managers.nix
@@ -0,0 +1,347 @@
+{
+  inputs,
+  lib,
+  ...
+}:
+{
+  flake.modules.darwin.base = { pkgs, ... }: {
+    system.defaults.dock = {
+      autohide = true;
+      orientation = "bottom";
+      show-recents = false;
+      tilesize = 48;
+      slow-motion-allowed = true;
+      
+      wvous-tl-corner = 1;
+      wvous-tr-corner = 1;
+      wvous-bl-corner = 1;
+      wvous-br-corner = 1;
+    };
+    services.aerospace = {
+      enable = true;
+      settings = {
+        gaps = {
+          inner = {
+            horizontal = 12;
+            vertical = 12;
+          };
+          outer = {
+            left = 10;
+            right = 10;
+            top = 10;
+            bottom = 10;
+          };
+        };
+        after-startup-command = [
+          "exec-and-forget ${pkgs.jankyborders}/bin/borders active_color=0xebbcbaff inactive_color=0x00000000 width=10.0"
+        ];
+        mode.main.binding = {
+          cmd-1 = "workspace 1";
+          cmd-shift-1 = ["move-node-to-workspace 1" "workspace 1"];
+          cmd-2 = "workspace 2";
+          cmd-shift-2 = ["move-node-to-workspace 2" "workspace 2"];
+          cmd-3 = "workspace 3";
+          cmd-shift-3 = ["move-node-to-workspace 3" "workspace 3"];
+          cmd-4 = "workspace 4";
+          cmd-shift-4 = ["move-node-to-workspace 4" "workspace 4"];
+          cmd-5 = "workspace 5";
+          cmd-shift-5 = ["move-node-to-workspace 5" "workspace 5"];
+          cmd-6 = "workspace 6";
+          cmd-shift-6 = ["move-node-to-workspace 6" "workspace 6"];
+          cmd-7 = "workspace 7";
+          cmd-shift-7 = ["move-node-to-workspace 7" "workspace 7"];
+          cmd-8 = "workspace 8";
+          cmd-shift-8 = ["move-node-to-workspace 8" "workspace 8"];
+          cmd-9 = "workspace 9";
+          cmd-shift-9 = ["move-node-to-workspace 9" "workspace 9"];
+          alt-space = "layout floating tiling";
+          alt-enter = "fullscreen";
+          cmd-enter = "exec-and-forget ${pkgs.alacritty}/bin/alacritty";
+          cmd-shift-s = "exec-and-forget screencapture -i -c";
+          cmd-h = [];
+          cmd-alt-h = [];
+        };
+      };
+    };
+  };
+  flake.modules.nixos.base = {
+    imports = [
+      inputs.mango.nixosModules.mango
+    ];
+    programs.mango.enable = true;
+  };
+
+  flake.modules.homeManager.base =
+    {
+      pkgs,
+      linux,
+      ...
+    }:
+    lib.optionalAttrs linux {
+      imports = [
+        inputs.mango.hmModules.mango
+      ];
+      wayland.windowManager.mango = {
+        enable = true;
+
+        autostart_sh = ''
+          dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+          systemctl --user restart xdg-desktop-portal
+          systemctl --user restart xdg-desktop-portal-wlr
+        '';
+
+        settings = {
+          exec-once = [
+            "${pkgs.eww}/bin/eww open bar0"
+            "${pkgs.eww}/bin/eww open bar1"
+            "~/.config/mango/autostart.sh"
+            "${pkgs.mako}/bin/mako"
+            "${pkgs.swww}/bin/swww-daemon"
+          ];
+
+          exec = [
+            "swww img ${../wallpapers/flake.jpg}"
+          ];
+
+          # Window effect
+          blur = "0";
+          blur_layer = "0";
+          blur_optimized = "1";
+          blur_params_num_passes = 2;
+          blur_params_radius = 5;
+          blur_params_noise = 0.02;
+          blur_params_brightness = 0.9;
+          blur_params_contrast = 0.9;
+          blur_params_saturation = 1.2;
+
+          shadows = 0;
+          layer_shadows = 0;
+          shadow_only_floating = 1;
+          shadows_size = 10;
+          shadows_blur = 15;
+          shadows_position_x = 0;
+          shadows_position_y = 0;
+          shadowscolor = "0x000000ff";
+
+          border_radius = "15";
+          no_radius_when_single = "0";
+          focused_opacity = "1.0";
+          unfocused_opacity = "0.9";
+
+          # Animation Configuration(support type:zoom,slide)
+          # tag_animation_direction: 0-horizontal,1-vertical
+          animations = "1";
+          layer_animations = "1";
+          animation_type_open = "slide";
+          animation_type_close = "slide";
+          animation_fade_in = "1";
+          animation_fade_out = "1";
+          tag_animation_direction = "1";
+          zoom_initial_ratio = "0.3";
+          zoom_end_ratio = "0.8";
+          fadein_begin_opacity = "0.5";
+          fadeout_begin_opacity = "0.8";
+          animation_duration_move = "500";
+          animation_duration_open = "400";
+
+          animation_duration_close = "800";
+          animation_duration_focus = "0";
+          animation_curve_open = "0.46,1.0,0.29,1";
+          animation_curve_move = "0.46,1.0,0.29,1";
+          animation_curve_tag = "0.46,1.0,0.29,1";
+          animation_curve_close = "0.08,0.92,0,1";
+          animation_curve_focus = "0.46,1.0,0.29,1";
+
+          # Scroller Layout Setting
+          scroller_structs = "20";
+          scroller_default_proportion = "0.8";
+          scroller_focus_center = "0";
+          scroller_prefer_center = "0";
+          edge_scroller_pointer_focus = "1";
+          scroller_default_proportion_single = "1.0";
+          scroller_proportion_preset = "0.5,0.8,1.0";
+
+          # Master-Stack Layout Setting
+          new_is_master = "1";
+          default_mfact = "0.55";
+          default_nmaster = "1";
+          smartgaps = "0";
+
+          # Overview Setting
+          hotarea_size = "10";
+          enable_hotarea = "0";
+          ov_tab_mode = "0";
+          overviewgappi = "5";
+          overviewgappo = "30";
+
+          # Misc
+          no_border_when_single = "0";
+          axis_bind_apply_timeout = "100";
+          focus_on_activate = "1";
+          sloppyfocus = "1";
+          warpcursor = "1";
+          focus_cross_monitor = "0";
+          focus_cross_tag = "0";
+          enable_floating_snap = "0";
+          snap_distance = "30";
+          cursor_size = "24";
+          drag_tile_to_tile = "1";
+
+          # Trackpad
+          # need relogin to make it apply
+          disable_trackpad = "0";
+          tap_to_click = "1";
+          tap_and_drag = "1";
+          drag_lock = "1";
+          trackpad_natural_scrolling = "1";
+          left_handed = "0";
+          middle_button_emulation = "0";
+          swipe_min_threshold = "1";
+
+          # mouse
+          # need relogin to make it apply
+          disable_while_typing = 0;
+          mouse_natural_scrolling = "0";
+
+          # Appearance
+          gappih = "10";
+          gappiv = "10";
+          gappoh = "10";
+          gappov = "10";
+          scratchpad_width_ratio = "0.8";
+          scratchpad_height_ratio = "0.9";
+          borderpx = "5";
+          rootcolor = "0x201b14ff";
+          bordercolor = "0x00000000";
+          focuscolor = "0xebbcbaff";
+          maximizescreencolor = "0xf6c177ff";
+          urgentcolor = "0xeb6f92ff";
+          scratchpadcolor = "0x31748fff";
+          globalcolor = "0xc4a7e7ff";
+          overlaycolor = "0x9ccfd8ff";
+
+          # layout support:
+          # tile,scroller,grid,deck,monocle,center_tile,vertical_tile,vertical_scroller
+          tagrule = [
+            "id:1,layout_name:tile"
+            "id:2,layout_name:tile"
+            "id:3,layout_name:tile"
+            "id:4,layout_name:tile"
+            "id:5,layout_name:tile"
+          ];
+
+          # Key Bindings
+          # key name refer to `xev` or `wev` command output,
+          # mod keys name: super,ctrl,alt,shift,none
+          bind = [
+            # reload config
+            "Super,R,reload_config"
+
+            # "special" keys
+            "NONE,XF86AudioPlay,spawn,${pkgs.playerctl}/bin/playerctl play-pause"
+            "NONE,XF86AudioNext,spawn,${pkgs.playerctl}/bin/playerctl next"
+            "NONE,XF86AudioPrev,spawn,${pkgs.playerctl}/bin/playerctl previous"
+            "NONE,XF86AudioRaiseVolume,spawn,${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.5"
+            "NONE,XF86AudioLowerVolume,spawn,${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+            "NONE,XF86AudioMute,spawn,${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+            "NONE,XF86AudioMicMute,spawn,${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+            "NONE,XF86MonBrightnessUp,spawn,${pkgs.brightnessctl}/bin/brightnessctl set +5%"
+            "NONE,XF86MonBrightnessDown,spawn,${pkgs.brightnessctl}/bin/brightnessctl set 5%-"
+            "Super,space,spawn,${pkgs.tofi}/bin/tofi-drun --drun-launch=true"
+            "Super+SHIFT,S,spawn,${pkgs.hyprshot}/bin/hyprshot -m region --clipboard-only"
+            "Super,Return,spawn,${pkgs.alacritty}/bin/alacritty"
+
+            # menu and terminal
+            "Super,F,spawn,${pkgs.nautilus}/bin/nautilus"
+
+            # exit
+            "Super+Shift,Q,quit"
+            "Super,q,killclient,"
+
+            # swap window
+            "Super+SHIFT,Up,exchange_client,up"
+            "Super+SHIFT,Down,exchange_client,down"
+            "Super+SHIFT,Left,exchange_client,left"
+            "Super+SHIFT,Right,exchange_client,right"
+
+            # switch window status
+            "Super,g,toggleglobal,"
+            "ALT,Tab,toggleoverview,"
+            "ALT,space,togglefloating,"
+            "ALT,f,togglemaximizescreen,"
+            "ALT,Return,togglefullscreen,"
+            "Super+SHIFT,f,togglefakefullscreen,"
+            "Super,o,toggleoverlay,"
+            "Super+SHIFT,I,restore_minimized"
+            "ALT,z,toggle_scratchpad"
+
+            # tag switch
+            "CTRL+Super,Left,viewtoleft,0"
+            "CTRL+Super,Right,viewtoright,0"
+
+            "Super,1,view,1,0"
+            "Super,2,view,2,0"
+            "Super,3,view,3,0"
+            "Super,4,view,4,0"
+            "Super,5,view,5,0"
+
+            # tag: move client to the tag and focus it
+            # tagsilent: move client to the tag and not focus it
+            "Super+Shift,1,tag,1,0"
+            "Super+Shift,2,tag,2,0"
+            "Super+Shift,3,tag,3,0"
+            "Super+Shift,4,tag,4,0"
+            "Super+Shift,5,tag,5,0"
+
+            # monitor switch
+            "alt+shift,Left,focusmon,left"
+            "alt+shift,Right,focusmon,right"
+            "Super+Alt,Left,tagmon,left"
+            "Super+Alt,Right,tagmon,right"
+
+            # gaps
+            "ALT+SHIFT,X,incgaps,1"
+            "ALT+SHIFT,Z,incgaps,-1"
+            "ALT+SHIFT,R,togglegaps"
+
+            # movewin
+            "CTRL+SHIFT,Up,movewin,+0,-50"
+            "CTRL+SHIFT,Down,movewin,+0,+50"
+            "CTRL+SHIFT,Left,movewin,-50,+0"
+            "CTRL+SHIFT,Right,movewin,+50,+0"
+
+            # resizewin
+            "CTRL+ALT,Up,resizewin,+0,-50"
+            "CTRL+ALT,Down,resizewin,+0,+50"
+            "CTRL+ALT,Left,resizewin,-50,+0"
+            "CTRL+ALT,Right,resizewin,+50,+0"
+          ];
+
+          # Mouse Button Bindings
+          # NONE mode key only work in ov mode
+          mousebind = [
+            "Super,btn_left,moveresize,curmove"
+            "Super,btn_right,moveresize,curresize"
+          ];
+
+          # layer rule
+          layerrule = [
+            "animation_type_open:zoom,layer_name:tofi-drun"
+            "animation_type_close:zoom,layer_name:tofi-drun"
+          ];
+
+          # window rule
+          windowrule = [
+            "isfloating:1,height:398,width:700,offsety:99,offsetx:99,isglobal:1,isoverlay:1,title:Picture-in-Picture"
+          ];
+
+          monitorrule = [
+            # 1440p Monitor (Left): Positioned at 0, 0
+            "name:DP-3,scale:1,x:0,y:0,width:2560,height:1440,refresh:180.0"
+            # 1440p Monitor (Middle): Positioned at 2560, 0
+            "name:DP-2,scale:1,x:2560,y:0,width:2560,height:1440,refresh:180.0"
+          ];
+        };
+      };
+    };
+}
