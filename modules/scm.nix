22c2a66f02160f44a825873c9d96a9a42fd17172
Invra <identificationsucks@gmail.com>
2026-04-28 18:00:48 +1000

    add: git stuff back

--- modules/scm.nix
+++ modules/scm.nix
@@ -23,6 +23,27 @@
             core.quotepath = "off";
           };
         };
+        
+        git = {
+          enable = true;
+          settings = {
+            user = {
+              name = "Invra";
+              email = "identificationsucks@gmail.com";
+            };
+            alias = {
+              a = "add";
+              p = "push -v";
+              s = "status -s";
+              c = "commit -m";
+              b = "branch --all";
+              co = "checkout -b";
+              m = "commit --amend";
+            };
+            init.defaultBranch = "main";
+            core.quotepath = "off";
+          };
+        };
 
         mercurial = {
           enable = true;
