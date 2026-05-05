d3498b7599576d9a0dca0e24c2969015b7045763
Invra <identificationsucks@gmail.com>
2026-05-06 07:13:39 +1000

    chore: format

--- modules/services.nix
+++ modules/services.nix
@@ -64,7 +64,7 @@
           enable = true;
           settings = {
             # workaround for
-            # https://github.com/nix-community/home-manager/issues/632  
+            # https://github.com/nix-community/home-manager/issues/632
             program_options.file_manager = "${pkgs.nautilus}/bin/nautilus";
           };
         };
