d848bcd148af94e7ac522b8ce129bf5c2aea4afd
Invra <identificationsucks@gmail.com>
2026-05-06 07:27:21 +1000

    fix: attr name issue

--- modules/specialisation.nix
+++ modules/specialisation.nix
@@ -5,7 +5,7 @@
       specialisation.nvidia-gpu.configuration = {
         services.xserver.videoDrivers = [ "nvidia" ];
       };
-      obs-studio.package = pkgs.obs-studio.override {
+      programs.obs-studio.package = pkgs.obs-studio.override {
         cudaSupport = true;
       };
       nixpkgs.allowedUnfreePackages = [
