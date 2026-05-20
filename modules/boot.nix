02c963e941a2064c242a2f48ba8608ee58546bf6
Invra <identificationsucks@gmail.com>
2026-05-20 10:44:55 +1000

    linux sucks

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,7 +6,7 @@
     }:
     {
       boot = {
-        kernelPackages = pkgs.linuxPackages_zen;
+        kernelPackages = pkgs.linuxPackages_6_18;
 
         kernel.sysctl = {
           "vm.max_map_count" = 2147483642;
@@ -19,16 +19,10 @@
         };
 
         kernelParams = [
-          # Helpful for plymouth
           "splash"
-          # Quiet down log messages
           "quiet"
           "loglevel=3"
           "udev.log-priority=3"
-          # Trust CPU with certain things
-          "random.trust_cpu=on"
-          # To fix `can't set config #1` for Novation Launchpad Pro MK3
-          "usbcore.quirks=1235:012f:m"
         ];
 
         loader = {
@@ -62,8 +56,6 @@
         };
 
       };
-      # Disadbled because of slowness, and CPU has trust.
-      systemd.services.systemd-boot-random-seed.enable = false;
 
       # Greeter
       services.displayManager.gdm.enable = true;
