b710978e00cf1e587facbc3772d8af4f9e16bc92
Invra <identificationsucks@gmail.com>
2026-06-27 17:01:02 +1000

    clean up xdg portals

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -1,21 +1,18 @@
 { lib, ... }:
 {
-  flake.modules.nixos.base =
-    { pkgs, ... }:
-    {
-      xdg.portal = {
-        enable = true;
-        wlr.enable = true;
-        extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-        config = {
-          preferred = {
-            default = "gtk";
-            "org.freedesktop.impl.portal.ScreenCast" = "wlr";
-            "org.freedesktop.impl.portal.Screenshot" = "wlr";
-          };
+  flake.modules.nixos.base = {
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      config = {
+        preferred = {
+          default = "gtk";
+          "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+          "org.freedesktop.impl.portal.Screenshot" = "wlr";
         };
       };
     };
+  };
   flake.modules.homeManager.base =
     {
       config,
